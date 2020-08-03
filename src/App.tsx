e10ff0c76cf2133c6c1633c82ac58af31073dd0a
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-08-03 23:47:32 +0200

    Add Resume view to Router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TopMenu from 'components/TopMenu/TopMenu';
 import TopMenuContextProvider from 'contexts/TopMenuContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'views/Home/Home';
+import Resume from 'views/Resume/Resume';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
           <Switch>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/resume">
+              <Resume />
             </Route>
           </Switch>
         </div>
