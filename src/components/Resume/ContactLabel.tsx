a8e98d3acaaaf22c70f53a35763a275ddde04651
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-08-03 23:27:28 +0200

    Add ContactLabel component

---
+++ src/components/Resume/ContactLabel.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ContactLabel.scss';
+
+interface Props {
+  svgSrc: string,
+  label: string
+}
+
+const ContactLabel: React.FC<Props> = ({svgSrc, label}) => {
+  return (
+    <div className="contact-label-wrapper">
+      <img className="contact-label-icon" src={ svgSrc } alt="icon" />
+      <p className="contact-label">{ label }</p>
+    </div>
+  );
+}
+
+export default ContactLabel;
