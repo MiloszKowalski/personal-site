108f683ea7d0b1aa5237f09fbf57abdab080dc13
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-08-04 23:48:16 +0200

    Add resume sections and fill in experience

--- src/views/Resume/Resume.tsx
+++ src/views/Resume/Resume.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import ContactLabel from 'components/Resume/ContactLabel';
 import './Resume.scss';
 import GithubIcon from 'components/svg/icons/Github.svg';
@@ -7,13 +8,21 @@ import InstagramIcon from 'components/svg/icons/Instagram.svg';
 import GlobeIcon from 'components/svg/icons/Globe.svg';
 import PhoneIcon from 'components/svg/icons/Phone.svg';
 import EmailIcon from 'components/svg/icons/Email.svg';
+import ExperienceIcon from './svg/ExperienceIcon.svg';
+import TechStackIcon from './svg/TechStackIcon.svg';
+import SkillsIcon from './svg/SkillsIcon.svg';
+import FollowUPLogo from './svg/FollowUPLogo.svg';
+import { ReactComponent as ArrowIcon } from 'components/svg/icons/ArrowIcon.svg';
+import { ReactComponent as TimeLineEN } from './svg/TimeLineEN.svg';
+import { ReactComponent as TimeLinePL } from './svg/TimeLinePL.svg';
 import { ReactComponent as ResumeTop } from 'views/Resume/svg/ResumeTop.svg';
 
 const Resume: React.FC = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="view-wrapper resume">
       <section className="resume-top">
-        <header>
+        <header id="name">
           <h1>Miłosz Kowalski</h1>
           <h2>FULL STACK .NET DEVELOPER</h2>
         </header>
@@ -42,6 +51,45 @@ const Resume: React.FC = () => {
           <div className="resume-photo"></div>
         </div>
         <ResumeTop />
+      </section>
+      <section id="experience" className="resume-section">
+        <header>
+          <div className="resume-icon-wrapper header-wrapper">
+            <img id="experience-icon" src={ ExperienceIcon } alt="experience" />
+          </div>
+          <h1>{ t('resume.experience') }</h1>
+        </header>
+        <div id="experience" className="resume-section-content">
+          <div className="resume-timeline">
+            {  i18n.language === 'pl' ? (<TimeLinePL />) : (<TimeLineEN />) }
+          </div>
+          <div className="label-wrapper">
+            <div className="resume-icon-wrapper big-wrapper">
+              <img id="followup" src={ FollowUPLogo } alt="FollowUP" />
+            </div>
+            <div className="company-title">
+              <h4>followup.social</h4>
+              <h5>Junior Full Stack Developer</h5>
+            </div>
+            <ArrowIcon />
+          </div>
+        </div>
+      </section>
+      <section className="resume-section">
+        <header>
+          <div className="resume-icon-wrapper header-wrapper">
+            <img src={ TechStackIcon } alt="techstack" />
+          </div>
+          <h1>{ t('resume.techstack') }</h1>
+        </header>
+      </section>
+      <section className="resume-section">
+        <header>
+          <div className="resume-icon-wrapper header-wrapper">
+            <img src={ SkillsIcon } alt="skills" />
+          </div>
+          <h1>{ t('resume.skills') }</h1>
+        </header>
       </section>
     </div>
   );
