5dc8753834435dc894040fd8481b3ddaea7cac30
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-08-12 22:11:47 +0200

    Add modal and modal context reference to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import TopMenu from 'components/TopMenu/TopMenu';
 import TopMenuContextProvider from 'contexts/TopMenuContext';
+import Modal from 'components/Modal/Modal';
+import ModalContextProvider from 'contexts/ModalContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'views/Home/Home';
 import Resume from 'views/Resume/Resume';
@@ -8,19 +10,22 @@ import Resume from 'views/Resume/Resume';
 function App() {
   return (
     <TopMenuContextProvider>
-      <Router>
-        <div className="App">
-          <TopMenu />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/resume">
-              <Resume />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <ModalContextProvider>
+        <Router>
+          <div className="App">
+            <TopMenu />
+            <Modal />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/resume">
+                <Resume />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </ModalContextProvider>
     </TopMenuContextProvider>
   );
 }
