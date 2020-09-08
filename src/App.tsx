0d0bbd046542c1435020fd8118c1094631726b4f
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-09-08 17:01:57 +0200

    Add image modal context provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 import TopMenu from 'components/TopMenu/TopMenu';
 import TopMenuContextProvider from 'contexts/TopMenuContext';
 import Modal from 'components/Modal/Modal';
+import ImageModal from 'components/ImageModal/ImageModal';
 import ModalContextProvider from 'contexts/ModalContext';
+import ImageModalContextProvider from 'contexts/ImageModalContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'views/Home/Home';
 import Resume from 'views/Resume/Resume';
+import Portfolio from 'views/Portfolio/Portfolio';
 
 function App() {
   return (
     <TopMenuContextProvider>
       <ModalContextProvider>
-        <Router>
-          <div className="App">
-            <TopMenu />
-            <Modal />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/resume">
-                <Resume />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+        <ImageModalContextProvider>
+          <Router>
+            <div className="App">
+              <TopMenu />
+              <Modal />
+              <ImageModal />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/resume">
+                  <Resume />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </ImageModalContextProvider>
       </ModalContextProvider>
     </TopMenuContextProvider>
   );
