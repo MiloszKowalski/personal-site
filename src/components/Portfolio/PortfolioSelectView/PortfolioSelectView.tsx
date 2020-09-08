b304d84b085cf4a4c821e388b1cc09f2540bab33
Miłosz Kowalski <milosz_kowalski44@wp.pl>
2020-09-08 16:57:45 +0200

    Add portfolio select view

---
+++ src/components/Portfolio/PortfolioSelectView/PortfolioSelectView.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { ReactComponent as BackendIcon } from 'components/svg/icons/BackendIcon.svg';
+import { ReactComponent as FrontendIcon } from 'components/svg/icons/FrontendIcon.svg';
+import { ReactComponent as DesignIcon } from 'components/svg/icons/DesignIcon.svg';
+import './PortfolioSelectView.scss';
+
+const PortfolioSelectView: React.FC = () => {
+  const { url } = useRouteMatch();
+
+  return (
+    <div className="portfolio-select-wrapper">
+      <Link to={`${url}/backend`}>
+        <div className="portfolio-menu-card">
+          <div className="portfolio-menu-card-icon-wrapper">
+            <BackendIcon />
+          </div>
+          <h5>BACKEND</h5>
+        </div>
+      </Link>
+      <Link to={`${url}/frontend`}>
+        <div className="portfolio-menu-card">
+          <div className="portfolio-menu-card-icon-wrapper">
+            <FrontendIcon />
+          </div>
+          <h5>FRONTEND</h5>
+        </div>
+      </Link>
+      <Link to={`${url}/design`}>
+        <div className="portfolio-menu-card">
+          <div className="portfolio-menu-card-icon-wrapper">
+            <DesignIcon />
+          </div>
+          <h5>DESIGN</h5>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default PortfolioSelectView;
